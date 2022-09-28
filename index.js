d48634393350ea47671dbd23d3053dceb93c654b
Emiliano Obregon <aquetzalliobregon@gmail.com>
2022-09-28 14:27:15 -0500

    add ternary operator

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const app = express();
-
-/*Verbos HTTP
-GET
-POST
-PATCH
-PUT
-DELETE
-*/
+const {pokemon} = require('./pokedex.json');
 
 app.get("/", (req, res, next) => {
-    res.status(200);
-    res.send('Bienvenido');
+
+    return res.status(200).send("Bienvenidos al Pokedex");
+});
+app.get("/pokemon/", (req, res, next) => {
+    return res.status(200).send(pokemon)
+
+});
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
+    const id = req.params.id - 1
+    (id >= 0 && id <= 150) ? 
+        res.status(200).send(pokemon[req.params.id - 1]) : 
+        res.status(404).send("Pokemon no encontrado");
 });
 
+app.get("/pokemon/:name([A-Za-z]+)", (req, res, next) =>{
+    const name = req.params.name;
+    const pk = pokemon.filter( p => {
+        return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
+    });
+    console.log(pk);
+    (pk.length > 0) ? 
+        res.status(200).send(pk) : 
+        res.status(404).send('Pokemon no encontrado')
 
-app.listen(3000, () => {
+
+})
+app.listen(process.env.PORT || 3000, () => {
     console.log('server is runing...');
 });
