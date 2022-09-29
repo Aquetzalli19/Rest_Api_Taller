2a811955d03083478c310ae34f19c9361914736a
Emiliano Obregon <aquetzalliobregon@gmail.com>
2022-09-28 19:17:05 -0500

    add method POST

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const bodyParser = require('body-parser')
 const express = require('express');
 const app = express();
 const {pokemon} = require('./pokedex.json');
 
-app.get("/", (req, res, next) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
+app.get("/", (req, res, next) => {
     return res.status(200).send("Bienvenidos al Pokedex");
 });
+
+app.post('/pokemon', (req, res, next) => {
+    return res.status(200).send(req.body.name)
+})
 app.get("/pokemon/", (req, res, next) => {
     return res.status(200).send(pokemon)
 
@@ -20,7 +27,7 @@ app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
 app.get("/pokemon/:name([A-Za-z]+)", (req, res, next) =>{
     const name = req.params.name;
     const pk = pokemon.filter( p => {
-        return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
+        return (p.name.toUpperCase() == name.toUpperCase()) && p ;
     });
     console.log(pk);
     (pk.length > 0) ? 
